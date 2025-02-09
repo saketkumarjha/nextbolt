1b8fef53377f2eb73f753636ccb190be7455c672
saket <jhasaket99dbg@gmail.com>
2025-02-09 18:31:16 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// import { Inter } from "next/font/google";
+import { NextAuthProvider } from "./Provider";
+import Navbar from "../app/components/Navbar";
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -19,10 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="min-h-screen bg-white">
+        <NextAuthProvider>
+          <div className="max-w-3xl mx-auto">
+            <Navbar />
+            {children}
+          </div>
+        </NextAuthProvider>
       </body>
     </html>
   );
